9e134445a8a4507dffa13541fc3f91817f179b77
komalapa <komalapa@gmail.com>
2021-03-02 15:33:09 +0400

    fix logout redirect to home

--- vue-auth/src/App.vue
+++ vue-auth/src/App.vue
@@ -33,7 +33,7 @@ export default {
   methods: {
     logout: function () {
       this.$store.dispatch("logout").then(() => {
-        this.$router.push("/login");
+        this.$router.push("/");
       });
     },
     created: function () {
